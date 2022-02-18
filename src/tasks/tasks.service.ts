41a8c26080e97b3115b92eb8a2835f5b99496e4a
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-02-18 16:44:52 +0900

    fix/error message

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -37,7 +37,7 @@ export class TasksService {
   getTaskById(id: string): Task {
     const found = this.tasks.find((task) => task.id === id);
     if (!found) {
-      throw new NotFoundException();
+      throw new NotFoundException(`Task with ID "${id}" not found`);
     }
     return found;
   }
