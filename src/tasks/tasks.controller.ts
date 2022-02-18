09d4113a180240c43ab3c4722bd03600a0749d2f
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-02-18 14:23:30 +0900

    feature/updateTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Patch } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) { }
 
   @Get()
   getAllTasks(): Task[] {
@@ -25,5 +25,10 @@ export class TasksController {
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+    return this.tasksService.updateTaskStatus(id, status)
   }
 }
