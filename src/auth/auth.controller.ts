ee9098029bea4b31a5aaa92b83e13bfe14e153f3
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-03-06 16:33:03 +0900

    imp/Guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -17,10 +17,4 @@ export class AuthController {
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialDto);
   }
-
-  @Post('/test')
-  @UseGuards(AuthGuard())
-  test(@Req() req) {
-    console.log(req);
-  }
 }
