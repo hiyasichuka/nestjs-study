c099716e38a325826c238b54a3a2f190faef7842
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-03-12 16:31:26 +0900

    update/enableCors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
