0072c6688e5007ff2982a362f48564c3b48e920f
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-03-12 18:48:34 +0900

    ready4heroku

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,16 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
