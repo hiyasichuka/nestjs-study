c2fe2450e664a74f721586bcbb5ebc9d98bff1ec
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-03-12 15:37:27 +0900

    remove/cofigModule

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,7 +9,6 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
@@ -21,12 +20,7 @@ import { TasksService } from './tasks.service';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
-  constructor(
-    private tasksService: TasksService,
-    private configServcie: ConfigService,
-  ) {
-    console.log(configServcie.get('TEST_VALUE'));
-  }
+  constructor(private tasksService: TasksService) {}
 
   @Get()
   getTasks(
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { TasksController } from './tasks.controller';
@@ -7,11 +6,7 @@ import { TasksRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [
-    ConfigModule,
-    TypeOrmModule.forFeature([TasksRepository]),
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
