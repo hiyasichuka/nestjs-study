014a17b01dba5f92064052ce4f422324c9b55de6
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-03-12 16:06:07 +0900

    add/ConfigValidation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
